fb76d1840050982dc4516b8b29d57b85a1c19cf4
Blaxinoss <abdulllahismael078@gmail.com>
2025-02-12 09:11:53 +0200

    no login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,12 @@ import AllUsers from './components/DetailedUsers/AllUsers.jsx';
 import UserDetails from './components/DetailedUsers/UserDetails.jsx';
 import ErrorBoundary from './components/ErrorBoundary';
 import ExpenseManager from './components/Expenses/ExpenseManager';
-import Login from './components/Login.jsx';
 import Navbar from './components/Navbar';
 import Settings from './components/Settings';
 import SubscriptionPage from './components/SubscriptionPage.jsx';
 import Trainers from './components/Trainers/Trainers';
 import './i18n';
 import store from './store.js';
-import PrivateRoute from './utils/ProtectedRoute';
 
 function App() {
 
@@ -40,21 +38,18 @@ function App() {
 			<Provider store={store}>
 				<BrowserRouter>
 					<Routes>
-						<Route path="/" element={<Login />} />
 
-						<Route element={<PrivateRoute />}>
-							<Route element={<Navbar />}>
-								<Route path="/trainers" element={<Trainers />} />
-								<Route path="/trainees" element={<SubscriptionPage />} />
-								<Route path="/expenses" element={<ExpenseManager />} />
+						<Route element={<Navbar />}>
+							<Route path="/trainers" element={<Trainers />} />
+							<Route path="/trainees" element={<SubscriptionPage />} />
+							<Route path="/expenses" element={<ExpenseManager />} />
 
-								<Route path="/dashboard" element={<Dashboard />} />
-								<Route path='/allUsers' element={<AllUsers />} />
-								<Route path='/userDetails/:phone' element={<UserDetails />} />
-								<Route path="/settings" element={<Settings />} />
+							<Route path="/dashboard" element={<Dashboard />} />
+							<Route path='/allUsers' element={<AllUsers />} />
+							<Route path='/userDetails/:phone' element={<UserDetails />} />
+							<Route path="/settings" element={<Settings />} />
 
-								<Route path="*" element={<ErrorBoundary />} />
-							</Route>
+							<Route path="*" element={<ErrorBoundary />} />
 						</Route>
 
 					</Routes>
